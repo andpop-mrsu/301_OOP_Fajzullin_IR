7ed8ff39ce59b0cb5709c8459b2769e3650f13e6
Andrey Popov <andrvpopov@gmail.com>
2025-04-25 06:49:24 +0300

    Add task06

---
+++ Task04/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ks561/task04',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9238442d820f5459d601772177992c7d54d26d3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ks561/task04' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9238442d820f5459d601772177992c7d54d26d3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
